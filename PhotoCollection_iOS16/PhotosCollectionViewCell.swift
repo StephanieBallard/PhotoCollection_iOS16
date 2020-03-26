50c643485cba7a7d5cf028958eaca3b1905bbfc8
Stephanie Ballard <user.name>
2020-03-26 14:52:02 -0700

    storyboard set up

---
+++ PhotoCollection_iOS16/PhotosCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoCollection_iOS16
+//
+//  Created by Stephanie Ballard on 3/26/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
