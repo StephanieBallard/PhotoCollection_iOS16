d08f1be86bbfd8f5d989a94b55b50992477b6340
Stephanie Ballard <user.name>
2020-03-26 13:21:48 -0700

    photo data model created

---
+++ PhotoCollection_iOS16/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  PhotoCollection_iOS16
+//
+//  Created by Stephanie Ballard on 3/26/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
