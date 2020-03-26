ece3908b5e3a5bd89d81472b2ce3ada40a9e633e
Stephanie Ballard <user.name>
2020-03-26 15:00:03 -0700

    setting up photoDetailViewController

---
+++ PhotoCollection_iOS16/PhotoDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoDetailViewController.swift
+//  PhotoCollection_iOS16
+//
+//  Created by Stephanie Ballard on 3/26/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+
+    var photoController: PhotoController?
+    var photo: Photo?
+    var themeHelper: ThemeHelper?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
